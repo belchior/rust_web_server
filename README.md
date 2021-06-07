23972f62c90fc3bce12e62e3624117675d7ff56c
Belchior Oliveira <belchior@outlook.com>
2021-06-07 12:50:51 -0300

    project setup

---
+++ README.md
@@ -0,0 +1,15 @@
+
+
+## References
+
+Actix Web
+
+- https://docs.rs/actix-web/3.3.2/actix_web/
+- https://docs.rs/serde/1.0.126/serde/
+
+MongoDB
+
+- https://developer.mongodb.com/quickstart/rust-crud-tutorial/
+- https://docs.rs/bson/0.14.1/bson/index.html
+- https://docs.rs/mongodb/1.2.1/mongodb
+- https://docs.rs/bson/1.2.2/bson
---
+++ server/scripts/coverage.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# Prerequisites
+# rustup toolchain install nightly
+# rustup component add llvm-tools-preview
+# cargo install rustfilt cargo-binutils
+
+# cargo clean;
+
+RUSTFLAGS="-Zinstrument-coverage" LLVM_PROFILE_FILE="target/debug/rust_calc-%m.profraw" cargo +nightly test --tests;
+
+cargo +nightly profdata -- merge -sparse target/debug/rust_calc-*.profraw -o target/debug/rust_calc.profdata;
+
+cargo +nightly cov -- report \
+    --use-color  \
+    --ignore-filename-regex='/.cargo/registry' \
+    --ignore-filename-regex='.*_spec\.rs$' \
+    --instr-profile=target/debug/rust_calc.profdata \
+    --object target/debug/deps/rust_calc-f0967c694a838d0e;
+
+cargo +nightly cov -- show \
+    --use-color \
+    --ignore-filename-regex='/.cargo/registry' \
+    --ignore-filename-regex='.*_spec\.rs$' \
+    --instr-profile=target/debug/rust_calc.profdata \
+    --object target/debug/deps/rust_calc-f0967c694a838d0e \
+    --show-instantiations --show-line-counts-or-regions \
+    --Xdemangler=rustfilt \
+    --output-dir=./target/debug \
+    --format=html;
+
+echo "\n\nAll files can be found at /target/debug/coverage/src/<Filename>.html";
+echo "\nExample: $(pwd)/target/debug/coverage/src/main.rs.html\n\n";
+
+# Reference
+# https://doc.rust-lang.org/nightly/unstable-book/compiler-flags/source-based-code-coverage.html

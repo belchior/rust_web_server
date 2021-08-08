6311987ca3ed8940603366b9772ad3ad15d08c7d
Belchior Oliveira <belchior@outlook.com>
2021-08-08 18:18:26 -0300

    moved cors origin to environment variable

--- server/src/cors.rs
+++ server/src/cors.rs
@@ -1,9 +1,11 @@
 use actix_cors::Cors;
 use actix_web::http;
+use std::env;
 
 pub fn get_cors() -> Cors {
+  let origins = env::var("CORS_ALLOWED_ORIGINS").unwrap();
   Cors::default()
-    .allowed_origin("http://localhost:3000")
+    .allowed_origin(origins.as_str())
     .allowed_methods(vec!["GET", "POST"])
     .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
     .allowed_header(http::header::CONTENT_TYPE)

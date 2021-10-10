cf9ce827779f06dadfd02abf6628b8ee408df11a
Belchior Oliveira <belchior@outlook.com>
2021-10-10 19:13:23 -0300

    was fixed issue when running code coverage multiples times

--- server/scripts/test_coverage.sh
+++ server/scripts/test_coverage.sh
@@ -1,40 +1,44 @@
 #!/bin/sh
 
 # Prerequisites
+# cargo clean;
 # rustup toolchain install nightly
-# rustup component add llvm-tools-preview
+# rustup +nightly component add llvm-tools-preview
 # cargo install rustfilt cargo-binutils
 
-# cargo clean;
 
-RUST_PKG_NAME="$(grep 'name\s*=\s*"' Cargo.toml | sed -E 's/.*"(.*)"/\1/')"
-RUST_COVERAGE_ID="$(ls target/debug/deps/rust_web_server-*.d | sed -E 's/.*-(.*)\.d$/\1/')"
-RUSTFLAGS="-Zinstrument-coverage" LLVM_PROFILE_FILE="target/debug/$RUST_PKG_NAME-%m.profraw" cargo +nightly test --tests;
+PKG_NAME="$(grep 'name\s*=\s*"' Cargo.toml | sed -E 's/.*"(.*)"/\1/')"
+COVERAGE_PATH="target/debug/test_coverage"
 
-cargo +nightly profdata -- merge -sparse target/debug/$RUST_PKG_NAME-*.profraw -o target/debug/$RUST_PKG_NAME.profdata;
+mkdir -p "$COVERAGE_PATH"
+
+RUSTFLAGS="-Z instrument-coverage" LLVM_PROFILE_FILE="$COVERAGE_PATH/$PKG_NAME-%m.profraw" cargo +nightly test --tests;
+cargo +nightly profdata -- merge -sparse $COVERAGE_PATH/$PKG_NAME-*.profraw -o $COVERAGE_PATH/$PKG_NAME.profdata;
+
+COVERAGE_ID="$(ls target/debug/deps/rust_web_server-*.d | head -n1 | cut -d " " -f1 | sed -E 's/.*-(.*)\.d$/\1/')"
 
 cargo +nightly cov -- report \
     --use-color \
     --ignore-filename-regex='/rustc/.*' \
     --ignore-filename-regex='./*_spec\.rs$' \
     --ignore-filename-regex='/.cargo/registry' \
-    --instr-profile=target/debug/$RUST_PKG_NAME.profdata \
-    --object target/debug/deps/$RUST_PKG_NAME-$RUST_COVERAGE_ID;
+    --instr-profile=$COVERAGE_PATH/$PKG_NAME.profdata \
+    --object target/debug/deps/$PKG_NAME-$COVERAGE_ID;
 
 cargo +nightly cov -- show \
     --use-color \
     --ignore-filename-regex='/rustc/.*' \
     --ignore-filename-regex='./*_spec\.rs$' \
     --ignore-filename-regex='/.cargo/registry' \
-    --instr-profile=target/debug/$RUST_PKG_NAME.profdata \
-    --object target/debug/deps/$RUST_PKG_NAME-$RUST_COVERAGE_ID \
-    --show-instantiations --show-line-counts-or-regions \
+    --instr-profile=$COVERAGE_PATH/$PKG_NAME.profdata \
+    --object target/debug/deps/$PKG_NAME-$COVERAGE_ID \
+    --show-instantiations \
+    --show-line-counts-or-regions \
     --Xdemangler=rustfilt \
-    --output-dir=./target/debug \
+    --output-dir=$COVERAGE_PATH \
     --format=html;
 
-echo "\n\nAll files can be found at /target/debug/coverage/src/<Filename>.html";
-echo "\nExample: $(pwd)/target/debug/coverage/src/main.rs.html\n\n";
+echo "\n\nAll files can be found at:\n$(pwd)/$COVERAGE_PATH/coverage/index.html\n\n";
 
 # Reference
-# https://doc.rust-lang.org/nightly/unstable-book/compiler-flags/source-based-code-coverage.html
+# https://doc.rust-lang.org/nightly/unstable-book/compiler-flags/instrument-coverage.html

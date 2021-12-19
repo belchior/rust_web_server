c27125f24fd0954c5c2ef6b0009c0745b7c7ff32
Belchior Oliveira <belchior@outlook.com>
2021-12-19 20:31:33 -0300

    updated dependencies of the client

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,22 +1,12 @@
 module.exports = {
-  'env': {
-    'es6': true,
-    'browser': true,
-    'jest/globals': true
-  },
   'plugins': [
     'jest',
   ],
   'extends': [
-    'react-app',
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:jest/recommended'
   ],
-  'globals': {
-    'Atomics': 'readonly',
-    'SharedArrayBuffer': 'readonly',
-  },
   'parser': '@typescript-eslint/parser',
   'parserOptions': {
     'ecmaVersion': 2018,
@@ -26,7 +16,7 @@ module.exports = {
     'indent': ["error", 2, { SwitchCase: 1 }],
     'linebreak-style': ['error', 'unix'],
     'max-len': ['error', { code: 120, ignoreComments: true }],
-    // 'no-console': ["error", { allow: ["warn", "error"] }],
+    'no-console': ["error", { allow: ["warn", "error"] }],
     'no-console': 'off',
     'object-curly-spacing': ['error', 'always'],
     'quotes': ['error', 'single'],

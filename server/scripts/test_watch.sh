86c86b0fd8fe4c41d51aba6113d6f91218fe4d06
Belchior Oliveira <belchior@outlook.com>
2022-01-01 14:11:11 -0300

    improved project setup

--- server/scripts/test_watch.sh
+++ server/scripts/test_watch.sh
@@ -2,6 +2,8 @@
 
 # TODO find a way to run tests in parellel:
 # https://doc.rust-lang.org/book/ch11-02-running-tests.html#running-tests-in-parallel-or-consecutively
-cargo watch -w ./src -x 'test -- --test-threads=1'
+
 # To print logs
 # cargo watch -w ./src -x 'test -- --test-threads=1 --nocapture --color always'
+
+cargo watch -w ./src -x 'test -- --test-threads=1'

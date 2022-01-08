8ae1f5fa7d3b35977497ca32b78b8ccb45b7bdb7
Belchior Oliveira <belchior@outlook.com>
2022-01-08 18:52:41 -0300

    improved project setup

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,26 +1,27 @@
 module.exports = {
-  'plugins': [
+  plugins: [
     'jest',
   ],
-  'extends': [
+  extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:jest/recommended'
   ],
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaVersion': 2018,
-    'sourceType': 'module'
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
   },
-  'rules': {
-    'indent': ["error", 2, { SwitchCase: 1 }],
+  rules: {
+    indent: ['error', 2, { SwitchCase: 1 }],
     'linebreak-style': ['error', 'unix'],
     'max-len': ['error', { code: 120, ignoreComments: true }],
-    'no-console': ["error", { allow: ["warn", "error"] }],
-    'no-console': 'off',
+    'no-console': ['error', { allow: ['warn', 'error'] }],
     'object-curly-spacing': ['error', 'always'],
-    'quotes': ['error', 'single'],
-    'semi': ['error', 'always'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
     'no-unused-vars': 'off',
+    'quote-props': ['error', 'as-needed'],
+    'no-multiple-empty-lines': ['error', { max: 1 }]
   }
 };

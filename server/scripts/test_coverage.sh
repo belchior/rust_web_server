48dc47c38d75107b775eedd2a5d3b5daf27d461c
Belchior Oliveira <belchior@outlook.com>
2022-02-20 18:54:49 -0300

    fix code coverage generation issue

--- server/scripts/test_coverage.sh
+++ server/scripts/test_coverage.sh
@@ -2,20 +2,19 @@
 
 # Prerequisites
 # cargo clean;
-# rustup toolchain install nightly
-# rustup +nightly component add llvm-tools-preview
 # cargo install rustfilt cargo-binutils
-
+# rustup toolchain install $nightly
+# rustup +nightly component add llvm-tools-preview
 
 PKG_NAME="$(grep 'name\s*=\s*"' Cargo.toml | sed -E 's/.*"(.*)"/\1/')"
+COVERAGE_OUTPUT="coverage"
 COVERAGE_DATA="target/debug/coverage"
-COVERAGE_PATH="coverage"
 
+mkdir -p "$COVERAGE_OUTPUT"
 mkdir -p "$COVERAGE_DATA"
-
 rm -f "target/debug/deps/$PKG_NAME"*
 
-RUSTFLAGS="-Z instrument-coverage" LLVM_PROFILE_FILE="$COVERAGE_DATA/$PKG_NAME-%m.profraw" cargo +nightly test -- --test-threads=1;
+RUSTFLAGS="-C instrument-coverage" LLVM_PROFILE_FILE="$COVERAGE_DATA/$PKG_NAME-%m.profraw" cargo +nightly test -- --test-threads=1;
 cargo +nightly profdata -- merge -sparse $COVERAGE_DATA/$PKG_NAME-*.profraw -o $COVERAGE_DATA/$PKG_NAME.profdata;
 
 COVERAGE_ID="$(ls target/debug/deps/$PKG_NAME-*.d | head -n1 | cut -d " " -f1 | sed -E 's/.*-(.*)\.d$/\1/')"
@@ -42,10 +41,10 @@ cargo +nightly cov -- show \
     --show-instantiations \
     --show-line-counts-or-regions \
     --Xdemangler=rustfilt \
-    --output-dir=$COVERAGE_PATH \
+    --output-dir=$COVERAGE_OUTPUT \
     --format=html;
 
-echo "\n\nAll files can be found at:\n$(pwd)/$COVERAGE_PATH/index.html\n\n";
+echo "\n\nAll files can be found at:\n$(pwd)/$COVERAGE_OUTPUT/index.html\n\n";
 
 # Reference
 # https://doc.rust-lang.org/nightly/unstable-book/compiler-flags/instrument-coverage.html

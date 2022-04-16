22e4c45a29690d0445ae440e5916b3a9877bd538
Belchior Oliveira <belchior@outlook.com>
2022-04-16 08:13:09 -0300

    added env variable POSTGRES_CONNECTIONS_NUMBER

--- server/src/db.rs
+++ server/src/db.rs
@@ -11,11 +11,13 @@ pub async fn db_connection_poll() -> Result<Pool, tokio_postgres::Error> {
   pg_config.port(env::var("POSTGRES_PORT").unwrap().parse().unwrap());
   pg_config.dbname(env::var("POSTGRES_DB").unwrap().as_str());
 
+  let conn_number: usize = env::var("POSTGRES_CONNECTIONS_NUMBER").unwrap().parse().unwrap();
+
   let mgr_config = ManagerConfig {
     recycling_method: RecyclingMethod::Fast,
   };
   let mgr = Manager::from_config(pg_config, tokio_postgres::NoTls, mgr_config);
-  let pool = Pool::builder(mgr).max_size(4).build().unwrap();
+  let pool = Pool::builder(mgr).max_size(conn_number).build().unwrap();
 
   log::info!(
     "DB connection with `{}` stablished with success",
